8aed46dbaeb0db50697724a4e52dd14c8732a700
Rohit Agarwal <programmingwithrohit@gmail.com>
2020-08-24 23:50:40 +0530

    Added factory method pattern

---
+++ src/main/java/com/javamultiplex/pattern/creational/factory/Point.java
@@ -0,0 +1,33 @@
+package com.javamultiplex.pattern.creational.factory;
+
+/**
+ * @author Rohit Agarwal on 24/08/20 11:40 pm
+ * @copyright www.javamultiplex.com
+ */
+public class Point {
+    private final double x;
+    private final double y;
+
+    private Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public static class Factory {
+        public static Point newCartesianPoint(double x, double y) {
+            return new Point(x, y);
+        }
+
+        public static Point newPolarPoint(double rho, double theta) {
+            return new Point(rho * Math.sin(theta), rho * Math.cos(theta));
+        }
+    }
+}
---
+++ src/test/java/com/javamultiplex/pattern/creational/factory/PointTest.java
@@ -0,0 +1,25 @@
+package com.javamultiplex.pattern.creational.factory;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author Rohit Agarwal on 24/08/20 11:46 pm
+ * @copyright www.javamultiplex.com
+ */
+public class PointTest {
+
+    @Test
+    public void shouldCreateCartesianPoint() {
+        Point point = Point.Factory.newCartesianPoint(10, 20);
+        Assertions.assertEquals(10, point.getX());
+        Assertions.assertEquals(20, point.getY());
+    }
+
+    @Test
+    public void shouldCreatePolarPoint() {
+        Point point = Point.Factory.newPolarPoint(10, 20);
+        Assertions.assertEquals(10 * Math.sin(20), point.getX());
+        Assertions.assertEquals(10 * Math.cos(20), point.getY());
+    }
+}
