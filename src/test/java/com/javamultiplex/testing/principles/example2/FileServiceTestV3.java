ba76525890f1573ceffd833ad6d9c7d805a00f71
Rohit Agarwal <programmingwithrohit@gmail.com>
2020-09-05 21:11:01 +0530

    Code refactor

--- src/test/java/com/javamultiplex/testing/principles/example2/FileServiceTestV3.java
+++ src/test/java/com/javamultiplex/testing/principles/example2/FileServiceTestV3.java
@@ -14,8 +14,9 @@ import java.util.List;
  * @author Rohit Agarwal on 05/09/20 7:55 pm
  * @copyright www.javamultiplex.com
  */
-public class FileServiceTestV2 {
+public class FileServiceTestV3 {
 
+    private static final String DIRECTORY = "src/test/resources/dummy/";
     private FileService fileService;
 
     @BeforeEach
@@ -26,27 +27,30 @@ public class FileServiceTestV2 {
     @Test
     public void shouldWriteToFile() throws IOException {
         String text = "I love my india";
-        Path path1 = fileService.write("src/test/resources/dummy/", "file1.txt", text.getBytes());
+        Path path1 = fileService.write(DIRECTORY, "file1.txt", text.getBytes());
         Assertions.assertEquals(Paths.get("file1.txt"), path1.getFileName());
 
-        Path path2 = fileService.write("src/test/resources/dummy/", "file2.txt", text.getBytes());
+        Path path2 = fileService.write(DIRECTORY, "file2.txt", text.getBytes());
         Assertions.assertEquals(Paths.get("file2.txt"), path2.getFileName());
 
-        Path path3 = fileService.write("src/test/resources/dummy/", "file3.txt", text.getBytes());
+        Path path3 = fileService.write(DIRECTORY, "file3.txt", text.getBytes());
         Assertions.assertEquals(Paths.get("file3.txt"), path3.getFileName());
 
-        Path path4 = fileService.write("src/test/resources/dummy/another/", "file4.txt", text.getBytes());
+        Path path4 = fileService.write(DIRECTORY + "another/", "file4.txt", text.getBytes());
         Assertions.assertEquals(Paths.get("file4.txt"), path4.getFileName());
-        fileService.cleanDirectory("src/test/resources/dummy/");
     }
 
     @Test
     public void shouldListAllFilesInDirectory() throws IOException {
         String text = "I love my india";
-        fileService.write("src/test/resources/dummy/", "file1.txt", text.getBytes());
-        fileService.write("src/test/resources/dummy/", "file2.txt", text.getBytes());
-        List<Path> list1 = fileService.list("src/test/resources/dummy/");
+        fileService.write(DIRECTORY, "file1.txt", text.getBytes());
+        fileService.write(DIRECTORY, "file2.txt", text.getBytes());
+        List<Path> list1 = fileService.list(DIRECTORY);
         Assertions.assertEquals(2, list1.size());
-        fileService.cleanDirectory("src/test/resources/dummy/");
+    }
+
+    @AfterEach
+    public void tearDown() throws IOException {
+        fileService.cleanDirectory(DIRECTORY);
     }
 }
